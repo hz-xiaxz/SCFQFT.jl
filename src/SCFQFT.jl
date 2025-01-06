3126d6869022c7454d5a682536efd8db8c77d232
hz-xiaxz <1806656034@qq.com>
2025-01-05 20:48:01 -0800

    update

--- src/SCFQFT.jl
+++ src/SCFQFT.jl
@@ -1,18 +1,20 @@
 module SCFQFT
 using LinearAlgebra
-using FFTW
-using SpecialFunctions
+using GreenFunc, CompositeGrids
 
+const beta = 0.1 # in the unit of T_F
+const ω_mesh = MeshGrid.ImFreq(β, FERMION)
+const kx_mesh = SimpleGrid.Uniform([-Λ, Λ], 50)
+const ky_mesh = SimpleGrid.Uniform([-Λ, Λ], 50)
+const kz_mesh = SimpleGrid.Uniform([-Λ, Λ], 50)
 
 # Constants
 const gamma = [1 0; 0 -1]
 const deltam = [1 0; 0 1]
 # the cutoff constant, V should go to 0- for the renormalization procedure
-const Λ = 10
+const Λ = 2
 const d = 3
 const c = 2 * π^(d / 2) / (SpecialFunctions.gamma(d / 2) * (2π)^d) * Λ^(d - 2) / (d - 2)
-const k_list = -Λ:1:Λ
-const ω_list = [(2n + 1)π for n = -10:9]
 
 # Helper functions for energy calculations
 function ϵ_bar(v::Float64)
@@ -62,14 +64,24 @@ function F_mean(; ω::Float64, v::Float64, μ::Float64, Δ::Float64)
 end
 # above ref Hausmann 2003 (3.63)-(3.67)
 
-# compute M(K, Ω_n)
-function SCF()
-    # start from phase space component of G and F
-    for k in k_list
-        for ω in ω_list
+function Green(; v::Float64, μ::Float64, Δ::Float64)
+    G_n = MeshArray(1:2, 1:2, kx_mesh, ky_mesh, kz_mesh, ω_mesh; dtype = ComplexF64)
+    for ind in eachindex(G_n)
+        kx = G_n.mesh[3][ind[3]]
+        ky = G_n.mesh[4][ind[4]]
+        kz = G_n.mesh[5][ind[5]]
+        ω_n = G_n.mesh[6][ind[6]]
+        if ind[1] == 1 && ind[2] == 1
+            G_n[ind] = G_mean(ω = ω_n, v = v, μ = μ, Δ = Δ)
+        elseif ind[1] == 1 && ind[2] == 2
 
         end
     end
+end
+# compute M(K, Ω_n)
+function SCF()
+    # start from phase space component of G and F
+    # the green function has no spin index, but Nambu indices
 end
 
 end # module
