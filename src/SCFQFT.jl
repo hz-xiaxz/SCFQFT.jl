a8d2377ba9073d551e40dee2b2eef7016383d2f6
hz-xiaxz <1806656034@qq.com>
2025-01-05 18:47:23 -0800

    remove epsilon parameter

--- src/SCFQFT.jl
+++ src/SCFQFT.jl
@@ -1,27 +1,35 @@
 module SCFQFT
 using LinearAlgebra
+using FFTW
+using SpecialFunctions
 
 
 # Constants
 const gamma = [1 0; 0 -1]
 const deltam = [1 0; 0 1]
+# the cutoff constant, V should go to 0- for the renormalization procedure
+const Λ = 10
+const d = 3
+const c = 2 * π^(d / 2) / (SpecialFunctions.gamma(d / 2) * (2π)^d) * Λ^(d - 2) / (d - 2)
+const k_list = -Λ:1:Λ
+const ω_list = [(2n + 1)π for n = -10:9]
 
 # Helper functions for energy calculations
-function ϵ_bar(; ϵ::Float64, v::Float64)
-    ϵ + 4 / (3π * v)
+function ϵ_bar(v::Float64)
+    1 + 4 / (3π * v)
 end
 
-function E(; ϵ::Float64, v::Float64, μ::Float64, Δ::Float64)
-    √((ϵ_bar(ϵ = ϵ, v = v) - μ)^2 + Δ^2)
+function E(; v::Float64, μ::Float64, Δ::Float64)
+    √((ϵ_bar(v) - μ)^2 + Δ^2)
 end
 
 # Helper functions for quantum factors
-function usq(; ϵ::Float64, v::Float64, μ::Float64, Δ::Float64)
-    1 / 2 * (1 + (ϵ_bar(ϵ = ϵ, v = v) - μ) / (E(ϵ = ϵ, v = v, μ = μ, Δ = Δ) - μ))
+function usq(; v::Float64, μ::Float64, Δ::Float64)
+    1 / 2 * (1 + (ϵ_bar(v) - μ) / (E(v = v, μ = μ, Δ = Δ) - μ))
 end
 
-function vsq(; ϵ::Float64, v::Float64, μ::Float64, Δ::Float64)
-    1 / 2 * (1 - (ϵ_bar(ϵ = ϵ, v = v) - μ) / (E(ϵ = ϵ, v = v, μ = μ, Δ = Δ) - μ))
+function vsq(; v::Float64, μ::Float64, Δ::Float64)
+    1 / 2 * (1 - (ϵ_bar(v) - μ) / (E(v = v, μ = μ, Δ = Δ) - μ))
 end
 
 # Main exported functions
@@ -34,9 +42,9 @@ Parameters:
     μ::Float64 - chemical potential
     Δ::Float64 - gap parameter
 """
-function G_mean(; ω::Float64, ϵ::Float64, v::Float64, μ::Float64, Δ::Float64)
-    usq(ϵ = ϵ, v = v, μ = μ, Δ = Δ) / (-im * ω + E(ϵ = ϵ, v = v, μ = μ, Δ = Δ) - μ) +
-    vsq(ϵ = ϵ, v = v, μ = μ, Δ = Δ) / (im * ω + E(ϵ = ϵ, v = v, μ = μ, Δ = Δ) - μ)
+function G_mean(; ω::Float64, v::Float64, μ::Float64, Δ::Float64)
+    usq(v = v, μ = μ, Δ = Δ) / (-im * ω + E(v = v, μ = μ, Δ = Δ) - μ) +
+    vsq(v = v, μ = μ, Δ = Δ) / (im * ω + E(v = v, μ = μ, Δ = Δ) - μ)
 end
 
 """
@@ -48,12 +56,20 @@ Parameters:
     μ::Float64 - chemical potential
     Δ::Float64 - gap parameter
 """
-function F_mean(; ω::Float64, ϵ::Float64, v::Float64, μ::Float64, Δ::Float64)
-    -Δ / (2 * (E(ϵ = ϵ, v = v, μ = μ, Δ = Δ) - μ)) * (
-        1 / (-im * ω + E(ϵ = ϵ, v = v, μ = μ, Δ = Δ) - μ) +
-        1 / (im * ω + E(ϵ = ϵ, v = v, μ = μ, Δ = Δ) - μ)
-    )
+function F_mean(; ω::Float64, v::Float64, μ::Float64, Δ::Float64)
+    -Δ / (2 * (E(v = v, μ = μ, Δ = Δ) - μ)) *
+    (1 / (-im * ω + E(v = v, μ = μ, Δ = Δ) - μ) + 1 / (im * ω + E(v = v, μ = μ, Δ = Δ) - μ))
 end
 # above ref Hausmann 2003 (3.63)-(3.67)
+
+# compute M(K, Ω_n)
+function SCF()
+    # start from phase space component of G and F
+    for k in k_list
+        for ω in ω_list
+
+        end
+    end
+end
 
 end # module
